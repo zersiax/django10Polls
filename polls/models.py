726ef2207f94180c79d1666da4249dc50ee94855
Florian Beijers <florianbeijers@gmail.com>
2015-12-18 00:54:59 +0000

    added a few question and choice objcts, registered question model to admin site, created superuser

--- polls/models.py
+++ polls/models.py
@@ -1,12 +1,17 @@
 from django.db import models
 
-class question(models.Model):
+class Question(models.Model):
 
   question_text = models.CharField(max_length=200)
   pub_date = models.DateTimeField('date published')
+  def __str__(self):
+    return self.question_text 
   
-class choice(models.Model):
+class Choice(models.Model):
   question = models.ForeignKey(Question, on_delete=models.CASCADE)
- choice_text = models.CharField(max_length=200)
-votes = models.IntegerField(default=0)
+  choice_text = models.CharField(max_length=200)
+  votes = models.IntegerField(default=0)
+  def __str__(self):
+    return self.choice_text
+  
 
