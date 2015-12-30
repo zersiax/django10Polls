69058f7b17d8e09a88bd83c6728ab01b6a83bfe7
Florian Beijers <florianbeijers@gmail.com>
2015-12-30 16:57:55 +0000

    refactored urls.py and views.py to use generic views, squashed some ugs, wrote failing test

--- polls/tests.py
+++ polls/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
+import datetime
+from django.utils import timezone
 
-# Create your tests here.
+from .models import Question
+class QuestionMethodTests(TestCase):
+  def test_was_published_recently_with_future_question(self):
+    # tests shouldn't return true for published_recently when the pub_date is in the future
+    time = timezone.now() + datetime.timedelta(days=30)
+    future_question = Question(pub_date=time)
+    self.assertEqual(future_question.was_published_recently(), False)
+	
