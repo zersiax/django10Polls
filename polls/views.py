bec5787e5cf66979cbc69355aacaeb3d20f46645
Florian Beijers <florianbeijers@gmail.com>
2015-12-30 13:35:46 +0000

    implemented get_object_or_404 shortcut for earlier 404 try-catch to make the code more subsinct and readable

--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
-from django.http import Http404
+
 from .models import Question
 
 def index(request):
@@ -9,10 +9,7 @@ def index(request):
   return render(request, 'polls/index.html', context)
 
 def detail(request, question_id):
-  try:
-    question = Question.objects.get(pk=question_id)
-  except Question.DoesNotExist:
-    raise Http404("That question has not been asked yet. Are you from the future?") 
+  question = get_object_or_404(Question, pk=question_id)
   return render(request, 'polls/detail.html', {'question': question})
   
 def results(request, question_id):
